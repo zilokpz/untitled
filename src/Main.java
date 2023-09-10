082fbe8102a4712ae13fe3b220858d47930ba4ec
666 <maxx19911ua@gmail.com>
2023-09-10 17:34:02 +0200

    test 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Павлюк Максим Евгениевич группа ПЗ-20-1/9 ");
+        System.out.println(LocalDate.now());
     }
 }
